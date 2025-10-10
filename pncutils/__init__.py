13460aa989edf28315ec4ee051481264b5073baf
yosukefk <yosukesabai@hotmail.com>
2025-10-10 12:44:10 -0500

    list getting too long

--- pncutils/__init__.py
+++ pncutils/__init__.py
@@ -1,2 +1,7 @@
-from . import a24, a24spc, annual_stats, camx_el2lo, camx_elwindow, camx_elpunchhole, camx_fixlo, camx_mrguam, camx_ptsmrg, camx_window, diff_stats, hourly, mda1, mda8, periodic_stats, tiftemplate
+from . import (
+        a24, a24spc, annual_stats, camx_el2lo, camx_elwindow, 
+        camx_elpunchhole, camx_fixlo, camx_mrguam, camx_ptsmrg, 
+        camx_window, diff_stats, hourly, mda1, mda8, periodic_stats, 
+        tiftemplate,
+        )
 
